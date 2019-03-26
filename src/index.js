52034ff7bb33e7ae4d5045d0c8d6fabc68c666aa
juunone <juunone@gmail.com>
2019-03-26 22:52:39 +0900

    delete serviceworker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import './index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
 
 
 const render = (Component) => { 
@@ -15,11 +14,4 @@ ReactDOM.render(
 
 render(App);
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-// Webpack Hot Module Replacement API 부분
 if (module.hot) module.hot.accept('./components/App', () => render(App));
