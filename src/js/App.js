6e8b69a3df4d840235425449d807ad6ea0d6bff0
juunone <juunone@gmail.com>
2019-07-28 18:44:10 +0900

    Added eslint

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, HashRouter as Router, Route } from 'react-router-dom';
 import importedComponent from 'react-imported-component';
 import Home from './Home';
--- src/js/Layout.js
+++ src/js/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Header, Container, Divider, Icon } from 'semantic-ui-react';
 
@@ -20,3 +21,7 @@ const Layout = ({ children }) => {
 };
 
 export default Layout;
+
+Layout.propTypes = {
+  children: PropTypes.array
+};
